7be94068ab7980573cbf3f51a9d1b49a2dadb80f
Sandeep <sandeep.bhatt59@gmail.com>
2018-01-19 15:44:06 +0545

    - Angular JS Course Completed.

--- Specialization Courses/Full Stack Web and Multiplatform Mobile App Development/FrontEnd JavaScript Frameworks  Angular/week1/conFusion/src/app/app.module.ts
+++ Specialization Courses/Full Stack Web and Multiplatform Mobile App Development/FrontEnd JavaScript Frameworks  Angular/week1/conFusion/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { MaterialModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
-
+import { MenuComponent } from './menu/menu.component';
+import { DishDetailComponent } from './dishdetail/dishdetail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    DishDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Specialization Courses/Full Stack Web and Multiplatform Mobile App Development/FrontEnd JavaScript Frameworks  Angular/week3/conFusion/src/app/app.module.ts
@@ -0,0 +1,69 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { MatToolbarModule, MatListModule, MatGridListModule, MatButtonModule, MatCardModule,
+  MatDialogModule, MatCheckboxModule, MatFormFieldModule, MatInputModule,
+  MatSelectModule, MatSlideToggleModule, MatProgressSpinnerModule, MatSliderModule } from '@angular/material'; 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms'; 
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { DishDetailComponent } from './dishdetail/dishdetail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
+import { LeaderService } from './services/leader.service';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    DishDetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,    
+    MatToolbarModule, 
+    MatListModule,   
+    MatGridListModule, 
+    MatButtonModule,
+    MatCardModule,
+    MatDialogModule,
+    MatCheckboxModule,
+    MatSelectModule, 
+    MatSlideToggleModule,
+    MatProgressSpinnerModule,
+    MatSliderModule,
+    FlexLayoutModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
+  providers: [
+    DishService,
+    PromotionService,
+    LeaderService
+  ],
+  entryComponents: [
+    LoginComponent
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
