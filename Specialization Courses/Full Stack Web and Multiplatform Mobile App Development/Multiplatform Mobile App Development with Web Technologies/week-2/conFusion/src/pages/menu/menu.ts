c59f890b7f21e2a76c03da3357f9087ff0c0d9ea
Sandeep <sandeep.bhatt59@gmail.com>
2018-05-26 09:14:47 +0545

    - Week 2 Assignment Completed. Ionic and Cordova

---
+++ Specialization Courses/Full Stack Web and Multiplatform Mobile App Development/Multiplatform Mobile App Development with Web Technologies/week-2/conFusion/src/pages/menu/menu.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+import { Dish } from '../../shared/dish';
+import { DishProvider } from '../../providers/dish/dish';
+import { DishdetailPage } from '../dishdetail/dishdetail';
+import { FavoriteProvider } from '../../providers/favorite/favorite';
+
+/**
+ * Generated class for the MenuPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+@IonicPage()
+@Component({
+  selector: 'page-menu',
+  templateUrl: 'menu.html',
+})
+export class MenuPage implements OnInit {
+  dishes: Dish[];
+  errMess: string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    private dishservice: DishProvider,
+    private favoriteservice: FavoriteProvider,
+    private toastCtrl: ToastController,
+    @Inject('BaseURL') private BaseURL ) { }
+  
+  ngOnInit() {
+    this.dishservice.getDishes()
+      .subscribe(dishes => this.dishes = dishes,
+        errmess => this.errMess = <any>errmess);
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad MenuPage');
+  }
+
+  dishSelected(event, dish) {
+    // That's right, we're pushing to ourselves!
+    this.navCtrl.push(DishdetailPage, {
+      dish: dish
+    });
+  }
+
+  addToFavorites(dish: Dish) {
+    console.log('Adding to Favorites', dish.id);
+    this.favoriteservice.addFavorite(dish.id);
+    this.toastCtrl.create({
+      message: 'Dish ' + dish.id + ' added as a favorite successfully',
+      duration: 3000
+    }).present();
+  }
+}
