ca1fa67515d6379ee37de0cf00b563768d3e807e
Sandeep <sandeep.bhatt59@gmail.com>
2018-05-30 15:01:07 +0545

    Ionic Split Pane, Grid and Storage, and Deploying your App Week 3 Assignment Completed.

---
+++ Specialization Courses/Full Stack Web and Multiplatform Mobile App Development/Multiplatform Mobile App Development with Web Technologies/week-3/conFusion/src/app/app.module.ts
@@ -0,0 +1,72 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { IonicStorageModule } from '@ionic/storage';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { AboutPage } from '../pages/about/about';
+import { MenuPage } from '../pages/menu/menu';
+import { ContactPage } from '../pages/contact/contact';
+import { DishdetailPage } from '../pages/dishdetail/dishdetail';
+import { FavoritesPage } from '../pages/favorites/favorites';
+import { ReservationPage } from '../pages/reservation/reservation';
+import { CommentPage } from '../pages/comment/comment';
+import { LoginPage } from '../pages/login/login';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { DishProvider } from '../providers/dish/dish';
+import { LeaderProvider } from '../providers/leader/leader';
+import { PromotionProvider } from '../providers/promotion/promotion';
+import { ProcessHttpmsgProvider } from '../providers/process-httpmsg/process-httpmsg';
+
+import { baseURL } from '../shared/baseurl';
+import { FavoriteProvider } from '../providers/favorite/favorite';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    AboutPage,
+    MenuPage,
+    ContactPage,
+    DishdetailPage,
+    FavoritesPage,
+    ReservationPage,
+    CommentPage,
+    LoginPage
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    AboutPage,
+    MenuPage,
+    ContactPage,
+    DishdetailPage,
+    FavoritesPage,
+    ReservationPage,
+    CommentPage,
+    LoginPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    DishProvider,
+    LeaderProvider,
+    PromotionProvider,
+    ProcessHttpmsgProvider,
+    { provide: 'BaseURL', useValue: baseURL },
+    FavoriteProvider
+  ]
+})
+export class AppModule {}
---
+++ Specialization Courses/Full Stack Web and Multiplatform Mobile App Development/Multiplatform Mobile App Development with Web Technologies/week-3/conFusion/src/providers/favorite/favorite.ts
@@ -0,0 +1,63 @@
+import { Http, Response } from '@angular/http'
+import { Injectable } from '@angular/core';
+import { Dish } from '../../shared/dish';
+import { Observable } from 'rxjs/Observable';
+import { DishProvider } from '../dish/dish';
+import { Storage } from '@ionic/storage';
+
+/*
+  Generated class for the FavoriteProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class FavoriteProvider {
+  
+  favorites: Array<any>;
+  
+  constructor(public http: Http, private dishservice: DishProvider,
+    private storage: Storage) {
+    console.log('Hello FavoriteProvider Provider');
+    storage.get('favorites').then(favorites => {
+      if (favorites) {
+        console.log(favorites);
+        this.favorites = favorites;
+      }
+      else {
+        this.favorites = [];
+      }
+    });
+  }
+
+  addFavorite(id: number): boolean {
+    if (!this.isFavorite(id)) {
+      this.favorites.push(id);
+      this.storage.set('favorites', this.favorites);
+    }      
+    console.log('favorites', this.favorites);
+    return true;
+  }
+
+  isFavorite(id: number): boolean {
+        return this.favorites.some(el => el === id);
+  }
+
+  getFavorites(): Observable<Dish[]> {
+    return this.dishservice.getDishes()
+      .map(dishes => dishes.filter(dish => this.favorites.some(el => el === dish.id)));
+  }
+
+  deleteFavorite(id: number): Observable<Dish[]> {
+    let index = this.favorites.indexOf(id);
+    if (index >= 0) {
+      this.favorites.splice(index,1);
+      this.storage.set('favorites', this.favorites);
+      return this.getFavorites();
+    }
+    else {
+      console.log('Deleting non-existant favorite', id);
+      return Observable.throw('Deleting non-existant favorite' + id);
+    }
+  }
+}
