202abf562bf82a24cdc4ead0f2f9e16c0317a289
Sandeep <sandeep.bhatt59@gmail.com>
2018-05-30 15:21:38 +0545

    Assignment 4: Ionic Native and Cordova Completed.

---
+++ Specialization Courses/Full Stack Web and Multiplatform Mobile App Development/Multiplatform Mobile App Development with Web Technologies/week-4/conFusion/src/providers/favorite/favorite.ts
@@ -0,0 +1,71 @@
+import { Http, Response } from '@angular/http'
+import { Injectable } from '@angular/core';
+import { Dish } from '../../shared/dish';
+import { Observable } from 'rxjs/Observable';
+import { DishProvider } from '../dish/dish';
+import { Storage } from '@ionic/storage';
+import { LocalNotifications } from '@ionic-native/local-notifications';
+
+/*
+  Generated class for the FavoriteProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class FavoriteProvider {
+  
+  favorites: Array<any>;
+  
+  constructor(public http: Http, private dishservice: DishProvider,
+    private storage: Storage,
+    private localNotifications: LocalNotifications) {
+    console.log('Hello FavoriteProvider Provider');
+    storage.get('favorites').then(favorites => {
+      if (favorites) {
+        console.log(favorites);
+        this.favorites = favorites;
+      }
+      else {
+        this.favorites = [];
+      }
+    });
+  }
+
+  addFavorite(id: number): boolean {
+    if (!this.isFavorite(id)) {
+      this.favorites.push(id);
+      this.storage.set('favorites', this.favorites);
+
+      // Schedule a single notification
+      this.localNotifications.schedule({
+        id: id,
+        text: 'Dish ' + id + 'added as a favorite successfully'
+      });
+    }      
+    console.log('favorites', this.favorites);
+    return true;
+  }
+
+  isFavorite(id: number): boolean {
+        return this.favorites.some(el => el === id);
+  }
+
+  getFavorites(): Observable<Dish[]> {
+    return this.dishservice.getDishes()
+      .map(dishes => dishes.filter(dish => this.favorites.some(el => el === dish.id)));
+  }
+
+  deleteFavorite(id: number): Observable<Dish[]> {
+    let index = this.favorites.indexOf(id);
+    if (index >= 0) {
+      this.favorites.splice(index,1);
+      this.storage.set('favorites', this.favorites);
+      return this.getFavorites();
+    }
+    else {
+      console.log('Deleting non-existant favorite', id);
+      return Observable.throw('Deleting non-existant favorite' + id);
+    }
+  }
+}
