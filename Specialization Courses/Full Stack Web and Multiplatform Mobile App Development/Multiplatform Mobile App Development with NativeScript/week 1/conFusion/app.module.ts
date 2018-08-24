d45a8b5a5408e0cd8a08d8bf9730caabcf0ba2cd
Sandeep <sandeep.bhatt59@gmail.com>
2018-08-24 13:29:55 +0545

    week 1 assignment completed

---
+++ Specialization Courses/Full Stack Web and Multiplatform Mobile App Development/Multiplatform Mobile App Development with NativeScript/week 1/conFusion/app.module.ts
@@ -0,0 +1,67 @@
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+import { NativeScriptHttpModule } from "nativescript-angular/http";
+import { AppRoutingModule } from "./app.routing";
+import { NativeScriptUISideDrawerModule } from "nativescript-telerik-ui/sidedrawer/angular";
+import { TNSFontIconModule } from 'nativescript-ngx-fonticon';
+
+import { AppComponent } from "./app.component";
+
+import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { DrawerComponent } from "./shared/drawer/drawer.component";
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+
+import { DishService } from './services/dish.service'; 
+import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
+import { baseURL } from './shared/baseurl';
+import { PromotionService } from "./services/promotion.service";
+import { LeaderService } from "./services/leader.service";
+
+
+// Uncomment and add to NgModule imports if you need to use two-way binding
+// import { NativeScriptFormsModule } from "nativescript-angular/forms";
+
+// Uncomment and add to NgModule imports  if you need to use the HTTP wrapper
+// import { NativeScriptHttpModule } from "nativescript-angular/http";
+
+@NgModule({
+    bootstrap: [
+        AppComponent
+    ],
+    imports: [
+        NativeScriptModule,
+        AppRoutingModule,
+        NativeScriptHttpModule,
+        NativeScriptUISideDrawerModule,
+        TNSFontIconModule.forRoot({ 
+            'fa': './fonts/font-awesome.min.css' 
+        })
+        
+    ],
+    declarations: [
+        AppComponent,
+        MenuComponent,
+        DishdetailComponent,
+        DrawerComponent,
+        HomeComponent,
+        ContactComponent,
+        AboutComponent
+    ],
+    providers: [
+        {provide: 'BaseURL', useValue: baseURL}, 
+        DishService, 
+        ProcessHTTPMsgService,
+        PromotionService,
+        LeaderService       
+    ],
+    schemas: [
+        NO_ERRORS_SCHEMA
+    ]
+})
+/*
+Pass your application module to the bootstrapModule function located in main.ts to start your app
+*/
+export class AppModule { }
---
+++ Specialization Courses/Full Stack Web and Multiplatform Mobile App Development/Multiplatform Mobile App Development with NativeScript/week 1/conFusion/shared/comment.ts
@@ -0,0 +1,7 @@
+export interface Comment 
+{ 
+    rating: number; 
+    comment: string; 
+    author: string; 
+    date: string;
+}
